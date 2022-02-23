4683d9627a34776e0ffb31bb28554e3e4eb8bba6
Chris Agbolade <yemluck@gmail.com>
2022-02-23 16:08:30 -0600

    created migrations for PetOwners

---
+++ Migrations/20220223215954_CreatePetOwnersTable.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotnet_bakery.Migrations
+{
+    public partial class CreatePetOwnersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "email",
+                table: "petOwners",
+                newName: "emailAddress");
+
+            migrationBuilder.AddColumn<int>(
+                name: "petCount",
+                table: "petOwners",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "petCount",
+                table: "petOwners");
+
+            migrationBuilder.RenameColumn(
+                name: "emailAddress",
+                table: "petOwners",
+                newName: "email");
+        }
+    }
+}
